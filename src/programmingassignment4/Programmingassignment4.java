aab9152801ff07c71a86b6e90dbff0f150587d10
Bailey Kouwenhoven <kouwenhoven.bailey@gmail.com>
2018-04-20 14:27:00 -0600

    still has main method working on pseudocode from the python example

--- src/programmingassignment4/Programmingassignment4.java
+++ src/programmingassignment4/Programmingassignment4.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package programmingassignment4;
 
 /**
- *
- * @author kouwe
+ *  Bailey Kouwenhoven and J Beckett Sweeney.
+ *   April 24, 2018
+ * Programming Assignment 4:  Dynamic programming coin problem
  */
 public class Programmingassignment4 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int amount = 63;
+        int[] array = {1,5,10,21,25};
+        int[] coinsUsed = new int[100];
+        int[] coinCounts = new int[100];
+        
+        System.out.println("Making change for "+ amount);
+        System.out.println("They are: ");
+
     }
     
+    
+    
+    public static int[] makeChange(int[] coinList, int amt){
+       int[] mCoins = new int[amt +1];                   //keep track of min coin
+       int[] coinsUsed = new int [amt+1];               //keep track of coins needed for change
+   
+       /*  some pseudocode from the python example ... i think
+        for loop searches through all coins less than amt, stores in mCoin array
+       
+       */      
+        return coinsUsed;
+        
+        
+    }
 }
+    
+    
+    
+   
